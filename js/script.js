4a1125a8e3fa56d10d503b0d38bf81c88de8c694
Nichiporchik-Nastya <87989987+Nichiporchik-Nastya@users.noreply.github.com>
2021-10-12 01:12:51 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const schemeSvg = document.querySelector('.scheme_svg');
+const totalPriceTag = document.querySelector('.price_total');
+const menuButton = document.querySelector('.m-menu');
+const menu = document.querySelector('.menu');
+
+let cost = 800;
+let count;
+let totalPrice = 0;
+
+schemeSvg.addEventListener('click', (event) => {
+    if(!event.target.classList.contains('booked')) event.target.classList.toggle('active');
+    count = schemeSvg.querySelectorAll('.active').length;
+    totalPrice = cost * count;
+    totalPriceTag.textContent = totalPrice;
+});
+
+menuButton.addEventListener('click', () =>{
+    menu.style.display = 'block';
+});
